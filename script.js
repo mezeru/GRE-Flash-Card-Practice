1bf491139cc1a88ee3d996eff00bfed70c5641d8
Mezeru <jermpro0@gmail.com>
2021-05-21 14:30:34 +0530

    prev

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ word = document.querySelector(".word")
 mean = document.querySelector(".mean")
 next = document.querySelector(".next")
 show = document.querySelector(".show")
+prev = document.querySelector(".prev")
 
+prev_no = null;
 keys = []
 let no = null;
 let Data = {}
@@ -17,8 +19,15 @@ $.getJSON("./content.json", function(data){
 next.addEventListener("click",() =>{
 
     if(no == null){
+        prev.style.display = "flex";
         next.innerHTML = "Next";
         show.style.display = "flex"
+    }else{
+        prev_no = no;
+    }
+
+    if(prev.style.display === "none"){
+        prev.style.display = "flex";
     }
 
     no = Math.floor(Math.random() * keys.length);
@@ -36,3 +45,13 @@ show.addEventListener("click",()=>{
         mean.style.display = "block";
     }
 })
+
+
+prev.addEventListener("click",()=>{
+
+    word.innerHTML = keys[prev_no];
+    mean.style.display = "none";
+    mean.innerHTML = Data[word.innerHTML]
+    prev.style.display = "none";
+
+});
